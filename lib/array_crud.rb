61ee221e79168600f0f795688d03c81c8c476008
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-15 22:13:33 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,21 +1,21 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  directors=["Orson Welles", "Ingmar Bergman", "Robert Bresson", "Federico Fellini"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  create_an_array << "Stanley Kubrick"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  create_an_array.unshift("Charlie Chaplin")
 end
 
 def remove_element_from_end_of_array(array)
-  
+  create_an_array.pop
 end
 
 def remove_element_from_start_of_array(array)
