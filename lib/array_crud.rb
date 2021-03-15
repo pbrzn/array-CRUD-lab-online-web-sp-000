cf0a7b28b9f6b8f828307c21f86e0cfc601d4036
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-15 22:17:36 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,11 +8,11 @@ end
 
 
 def add_element_to_end_of_array(array, element)
-  array << "Stanley Kubrick"
+  array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
-  array.unshift("Charlie Chaplin")
+  array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
