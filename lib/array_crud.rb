5cd0ee9b89e27ed8d6bae971b889e0f00ecb8dc8
jolly-technologist-1233 <pbrennanmusic@gmail.com>
2021-03-15 22:16:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,29 +8,29 @@ end
 
 
 def add_element_to_end_of_array(array, element)
-  create_an_array << "Stanley Kubrick"
+  array << "Stanley Kubrick"
 end
 
 def add_element_to_start_of_array(array, element)
-  create_an_array.unshift("Charlie Chaplin")
+  array.unshift("Charlie Chaplin")
 end
 
 def remove_element_from_end_of_array(array)
-  create_an_array.pop
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array.index(index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array.first
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array.last
 end
